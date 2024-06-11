47c6228fd57f3e9d152fface54ecf39f15c7a3e0
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-06-11 23:45:07 +0200

    First api version

--- demo/demo.py
+++ demo/demo.py
@@ -10,6 +10,7 @@ import warnings
 import cv2
 import tqdm
 import json
+from flask import Flask, request, jsonify
 
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
@@ -18,63 +19,125 @@ from detectron2.utils.logger import setup_logger
 from predictor import VisualizationDemo
 
 from detectron2.projects import point_rend
+
 # constants
 WINDOW_NAME = "COCO detections"
 
+app = Flask(__name__)
 
-def setup_cfg(args):
+def setup_cfg(config_file, opts, confidence_threshold):
     # load config from file and command-line arguments
     cfg = get_cfg()
     point_rend.add_pointrend_config(cfg)
-    # To use demo for Panoptic-DeepLab, please uncomment the following two lines.
-    # from detectron2.projects.panoptic_deeplab import add_panoptic_deeplab_config  # noqa
-    # add_panoptic_deeplab_config(cfg)
-    cfg.merge_from_file(args.config_file)
-    cfg.merge_from_list(args.opts)
+    cfg.merge_from_file(config_file)
+    cfg.merge_from_list(opts)
     # Set score_threshold for builtin models
-    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = args.confidence_threshold
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
-    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = confidence_threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = confidence_threshold
     cfg.freeze()
     return cfg
 
+@app.route('/process_images', methods=['POST'])
+def process_images():
+    data = request.json
+    config_file = data.get('config_file')
+    input_images = data.get('input_images')
+    confidence_threshold = data.get('confidence_threshold', 0.5)
+    opts = data.get('opts', [])
+    
+    if not config_file or not input_images:
+        return jsonify({"error": "config_file and input_images are required"}), 400
+    
+    cfg = setup_cfg(config_file, opts, confidence_threshold)
+    demo = VisualizationDemo(cfg)
+    
+    results = {}
+    
+    if len(input_images) == 1:
+        input_images = glob.glob(os.path.expanduser(input_images[0]))
+        assert input_images, "The input path(s) was not found"
+    if not os.path.exists("jsonFiles"):
+        os.makedirs("jsonFiles")
+    for path in tqdm.tqdm(input_images):
+        img = read_image(path, format="BGR")
+        start_time = time.time()
+        predictions, visualized_output = demo.run_on_image(img)
+        
+        image_results = {}
+        
+        if "panoptic_seg" in predictions: 
+            panoptic_masks = predictions["panoptic_seg"][0].tolist()
+            panoptic_classes = [seg_info["category_id"] for seg_info in predictions["panoptic_seg"][1]]
+            image_results["panoptic_masks"] = panoptic_masks
+            image_results["panoptic_classes"] = panoptic_classes
+        else:
+            contours = [cv2.findContours(pred_mask.numpy().astype('uint8'), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0][0].tolist() for pred_mask in predictions['instances'].pred_masks]
+            classes = predictions['instances'].pred_classes.tolist()
+            image_results["contours"] = contours
+            image_results["classes"] = classes
+        
+        results[path] = image_results
+        logger.info(
+            "{}: {} in {:.2f}s".format(
+                path,
+                "detected {} instances".format(len(predictions["instances"]))
+                if "instances" in predictions
+                else "finished",
+                time.time() - start_time,
+            )
+        )
+    return jsonify(results)
 
-def get_parser():
-    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
-    parser.add_argument(
-        "--config-file",
-        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
-        metavar="FILE",
-        help="path to config file",
+@app.route('/process_video', methods=['POST'])
+def process_video():
+    data = request.json
+    config_file = data.get('config_file')
+    video_input = data.get('video_input')
+    output = data.get('output')
+    confidence_threshold = data.get('confidence_threshold', 0.5)
+    opts = data.get('opts', [])
+    
+    if not config_file or not video_input:
+        return jsonify({"error": "config_file and video_input are required"}), 400
+    
+    cfg = setup_cfg(config_file, opts, confidence_threshold)
+    demo = VisualizationDemo(cfg)
+    
+    video = cv2.VideoCapture(video_input)
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frames_per_second = video.get(cv2.CAP_PROP_FPS)
+    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    basename = os.path.basename(video_input)
+    codec, file_ext = (
+        ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
     )
-    parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
-    parser.add_argument("--video-input", help="Path to video file.")
-    parser.add_argument(
-        "--input",
-        nargs="+",
-        help="A list of space separated input images; "
-        "or a single glob pattern such as 'directory/*.jpg'",
-    )
-    parser.add_argument(
-        "--output",
-        help="A file or directory to save output visualizations. "
-        "If not given, will show output in an OpenCV window.",
-    )
-
-    parser.add_argument(
-        "--confidence-threshold",
-        type=float,
-        default=0.5,
-        help="Minimum score for instance predictions to be shown",
-    )
-    parser.add_argument(
-        "--opts",
-        help="Modify config options using the command-line 'KEY VALUE' pairs",
-        default=[],
-        nargs=argparse.REMAINDER,
-    )
-    return parser
-
+    if codec == ".mp4v":
+        warnings.warn("x264 codec not available, switching to mp4v")
+    if output:
+        if os.path.isdir(output):
+            output_fname = os.path.join(output, basename)
+            output_fname = os.path.splitext(output_fname)[0] + file_ext
+        else:
+            output_fname = output
+        assert not os.path.isfile(output_fname), output_fname
+        output_file = cv2.VideoWriter(
+            filename=output_fname,
+            fourcc=cv2.VideoWriter_fourcc(*codec),
+            fps=float(frames_per_second),
+            frameSize=(width, height),
+            isColor=True,
+        )
+    assert os.path.isfile(video_input)
+    for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
+        if output:
+            output_file.write(vis_frame)
+    video.release()
+    if output:
+        output_file.release()
+    
+    return jsonify({"status": "processing completed", "output_file": output_fname if output else None})
 
 def test_opencv_video_format(codec, file_ext):
     with tempfile.TemporaryDirectory(prefix="video_format_test") as dir:
@@ -92,141 +155,8 @@ def test_opencv_video_format(codec, file_ext):
             return True
         return False
 
-
 if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
-    args = get_parser().parse_args()
     setup_logger(name="fvcore")
     logger = setup_logger()
-    logger.info("Arguments: " + str(args))
-
-    cfg = setup_cfg(args)
-
-    demo = VisualizationDemo(cfg)
-
-    if args.input:
-        if len(args.input) == 1:
-            args.input = glob.glob(os.path.expanduser(args.input[0]))
-            assert args.input, "The input path(s) was not found"
-        if not os.path.exists("jsonFiles"):
-            os.makedirs("jsonFiles")
-        for path in tqdm.tqdm(args.input, disable=not args.output):
-            # use PIL, to be consistent with evaluation
-            img = read_image(path, format="BGR")
-            start_time = time.time()
-            predictions, visualized_output = demo.run_on_image(img)
-            
-            if "panoptic_seg" in predictions: 
-                json_object = json.dumps(predictions["panoptic_seg"][0].tolist())
-                with open("jsonFiles\\panoptic_masks_{}.json".format(path.split("\\")[-1]), "w") as outfile:
-                   outfile.write(json_object)
-
-                classes = []
-                for seg_info in predictions["panoptic_seg"][1]:
-                    classes.append(seg_info["category_id"])
-                json_object = json.dumps(classes)
-                with open("jsonFiles\\panoptic_classes_{}.json".format(path.split("\\")[-1]), "w") as outfile:
-                   outfile.write(json_object)
-            else:
-                contours = []
-                for pred_mask in predictions['instances'].pred_masks:
-                    mask = pred_mask.numpy().astype('uint8')
-                    contour, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-                    contours.append(contour[0].tolist())
-                
-                json_object = json.dumps(contours)
-                with open("jsonFiles\\contours_{}.json".format(path.split("\\")[-1]), "w") as outfile:
-                    outfile.write(json_object)
-
-                classes = predictions['instances'].pred_classes.tolist()
-                
-                json_object = json.dumps(classes)
-                with open("jsonFiles\\classes_{}.json".format(path.split("\\")[-1]), "w") as outfile:
-                    outfile.write(json_object)
-                
-            
-            #boxes = []
-            #for pred_box in predictions['instances'].pred_boxes:
-            #    box = pred_box.numpy().astype('uint8')
-            #    boxes.append(box[0].tolist())
-            #    
-            #json_object = json.dumps(boxes)
-            #with open("boxes{}.json".format(path.split("\\")[-1]), "w") as outfile:
-            #    outfile.write(json_object)
-                
-            logger.info(
-                "{}: {} in {:.2f}s".format(
-                    path,
-                    "detected {} instances".format(len(predictions["instances"]))
-                    if "instances" in predictions
-                    else "finished",
-                    time.time() - start_time,
-                )
-            )
-                          
-            if args.output:
-                if os.path.isdir(args.output):
-                    assert os.path.isdir(args.output), args.output
-                    out_filename = os.path.join(args.output, os.path.basename(path))
-                else:
-                    assert len(args.input) == 1, "Please specify a directory with args.output"
-                    out_filename = args.output
-                visualized_output.save(out_filename)
-            else:
-                cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-                cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
-                if cv2.waitKey(0) == 27:
-                    break  # esc to quit
-    elif args.webcam:
-        assert args.input is None, "Cannot have both --input and --webcam!"
-        assert args.output is None, "output not yet supported with --webcam!"
-        cam = cv2.VideoCapture(0)
-        for vis in tqdm.tqdm(demo.run_on_video(cam)):
-            cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-            cv2.imshow(WINDOW_NAME, vis)
-            if cv2.waitKey(1) == 27:
-                break  # esc to quit
-        cam.release()
-        cv2.destroyAllWindows()
-    elif args.video_input:
-        video = cv2.VideoCapture(args.video_input)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        basename = os.path.basename(args.video_input)
-        codec, file_ext = (
-            ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
-        )
-        if codec == ".mp4v":
-            warnings.warn("x264 codec not available, switching to mp4v")
-        if args.output:
-            if os.path.isdir(args.output):
-                output_fname = os.path.join(args.output, basename)
-                output_fname = os.path.splitext(output_fname)[0] + file_ext
-            else:
-                output_fname = args.output
-            assert not os.path.isfile(output_fname), output_fname
-            output_file = cv2.VideoWriter(
-                filename=output_fname,
-                # some installation of opencv may not support x264 (due to its license),
-                # you can try other format (e.g. MPEG)
-                fourcc=cv2.VideoWriter_fourcc(*codec),
-                fps=float(frames_per_second),
-                frameSize=(width, height),
-                isColor=True,
-            )
-        assert os.path.isfile(args.video_input)
-        for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-            if args.output:
-                output_file.write(vis_frame)
-            else:
-                cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
-                cv2.imshow(basename, vis_frame)
-                if cv2.waitKey(1) == 27:
-                    break  # esc to quit
-        video.release()
-        if args.output:
-            output_file.release()
-        else:
-            cv2.destroyAllWindows()
+    app.run(host='0.0.0.0', port=5000)
